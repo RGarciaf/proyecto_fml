fa2893ad270b8b5005028b9dfc2adbaa40ffb51a
Coutrybee Rose <ramangf@gmail.com>
2018-12-12 14:26:46 +0000

    Creacion de dataset.py

---
+++ Dataset.py
@@ -0,0 +1,51 @@
+import numpy as np
+import letritas
+
+class Dataset():
+     def __init__(self):
+          self.datos_Bruto = []
+     
+     def recortar(self):
+          pass
+
+     def cuadraditos(self):
+          pass
+     
+     def cuadraditosTodos(self):
+          pass
+     
+     def cuadraditosBloques(self):
+          pass
+     
+     def cuadraditosColumnas(self):
+          pass
+     
+     def cuadraditosRandom(self):
+          pass
+     
+     def cuadraditosDiagonales(self):
+          pass
+     
+     def cuadraditosMaxiBloques(self):
+          pass
+     
+     def cuadraditosFilas(self):
+          pass
+     
+     def patrones(self):
+          pass
+     
+     def diferencia(self):
+          pass
+     
+     def diferenciaPixel(self):
+          pass
+     
+     def diferenciaPorcentaje(self):
+          pass
+     
+     def negros(self):
+          pass
+     
+     
+     
